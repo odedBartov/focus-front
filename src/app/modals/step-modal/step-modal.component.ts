9b3b89b12e8c12f6e57f48a698041c3bb6431773
tripper-app <tripper.app.il@gmail.com>
2025-04-09 00:12:05 +0300

    finish modal

--- src/app/modals/step-modal/step-modal.component.ts
+++ src/app/modals/step-modal/step-modal.component.ts
@@ -2,7 +2,7 @@ import { Component, inject, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Step } from '../../models/step';
 import { StepType } from '../../models/stepType';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HttpService } from '../../services/http.service';
 import { Project } from '../../models/project';
@@ -20,19 +20,26 @@ export class StepModalComponent {
   stepTypeEnum = StepType;
   stepTypes = Object.values(StepType);
   currentStep: Step;
+  isNewStep = false;
+
   constructor(@Inject(MAT_DIALOG_DATA) public data: { step: Step, project: Project }) {
     this.currentStep = data?.step ? { ...data.step } : new Step();
+    this.isNewStep = data?.step? true : false;
   }
 
-  save() {
-    if (this.data.step) {
-      this.httpServcie.updateStep(this.data.project, this.currentStep).subscribe(res => {
-        this.closeModal()
-      })
+  save(stepForm: NgForm) {
+    if (stepForm.valid) {
+      if (this.data.step) {
+        this.httpServcie.updateStep(this.data.project, this.currentStep).subscribe(res => {
+          this.closeModal()
+        })
+      } else {
+        this.httpServcie.createStep(this.data?.project, this.currentStep).subscribe(res => {
+          this.closeModal()
+        })
+      }
     } else {
-      this.httpServcie.createStep(this.data?.project, this.currentStep).subscribe(res => {
-        this.closeModal()
-      })
+      Object.values(stepForm.controls).forEach(control => control.markAsTouched());
     }
   }
 
