dd851f8ba64dba00c1ba9fcff27acb78c42816d3
oded_bartov <odedoded777@gmail.com>
2025-05-05 11:34:05 +0300

    version

--- src/app/new-step/new-step.component.ts
+++ src/app/new-step/new-step.component.ts
@@ -13,11 +13,19 @@ export class NewStepComponent implements OnInit {
   stepTypes: { text: string, icon: string, type: StepType }[] = []
   stepTypeLabels = stepTypeLabels;
   stepTypeEnum = StepType;
+  selectedType?: StepType;
+
   ngOnInit(): void {
     this.stepTypes = [
       { text: "שלב בפרויקט שצריך לבצע", icon: "working", type: StepType.task },
       { text: "על ידי הלקוח", icon: "dollar", type: StepType.payment },
       { text: "חוזה, פגישה, אימייל חשוב וכו'", icon: "telegram", type: StepType.coomunication }
     ];
+
+    this.selectedType = StepType.task// delete
+  }
+
+  selectType(type: StepType) {
+    this.selectedType = type;
   }
 }
