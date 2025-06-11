1ea46cc9284e0bd24a98c20a4b54acd3cca0b897
oded_bartov <odedoded777@gmail.com>
2025-06-11 11:53:09 +0300

    error message

--- src/app/modals/error/error.component.ts
+++ src/app/modals/error/error.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { AfterViewInit, Component, inject } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
   selector: 'app-error',
@@ -7,8 +8,26 @@ import { MatDialogRef } from '@angular/material/dialog';
   templateUrl: './error.component.html',
   styleUrl: './error.component.scss'
 })
-export class ErrorComponent {
+export class ErrorComponent implements AfterViewInit {
   dialogRef = inject(MatDialogRef<ErrorComponent>);
+  authenticationService = inject(AuthenticationService);
+  OdedsNumber = "972584046213";
+  fullName = "משתמש ללא שם";
+  
+  ngAfterViewInit(): void {
+    const userName = this.authenticationService.getUserName();
+    if (userName) { 
+      this.fullName = userName;
+    }
+  }
+
+  openWhatsapp() {
+    const message = `הי, זה ${this.fullName}. אני משתמש בפוקוס ונתקלתי בבעיה.
+    הנה הסבר מפורט של התקלה והצעדים שעשיתי שהובילו אליה:
+    `;
+    const url = `https://wa.me/${this.OdedsNumber}?text=${message}`;
+    window.open(url, '_blank');
+  }
 
   closeModal() {
     this.dialogRef.close();
