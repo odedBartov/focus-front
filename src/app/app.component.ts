1ae6bf8891d82122fc51b267996cde926f0a61f3
Oded Bartov <odedoded777@gmail.com>
2025-07-03 09:33:59 +0300

    clarity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -57,20 +57,8 @@ export class AppComponent implements OnInit {
     })
 
     this.initHotJar();
-    this.initClarity();
   }
 
-  initClarity() {
-    if (!environment.production) {
-      const clarityScript = document.createElement('script');
-      clarityScript.innerHTML = ` (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "s8hrq4wc0s");
-`;
-    }
-  }
   initHotJar() {
     if (!environment.production) {
       const hotjarScript = document.createElement('script');
