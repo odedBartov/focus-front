8311f31d17b09affb140088d39b7e19499af366e
Oded Bartov <odedoded777@gmail.com>
2025-07-23 14:50:36 +0300

    urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: 'https://focus-server-51qa.onrender.com/api/',
-    subscriptionUrl: 'https://arieladler.co.il/focus/',
+    subscriptionUrl: 'https://arieladler.co.il/focus/#3/',
     defaultPicture: 'assets/icons/default_profile.svg'
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
     apiUrl: 'https://localhost:4300/api/',
     realApi: 'https://focus-server-51qa.onrender.com/api/',
-    subscriptionUrl: 'https://arieladler.co.il/focus/',
+    subscriptionUrl: 'https://arieladler.co.il/focus/#3/',
     defaultPicture: 'assets/icons/default_profile.svg'
   };
