718b604a87eaad5c9e79d36e6f47573a1751bfc1
Oded Bartov <odedoded777@gmail.com>
2025-07-30 14:19:11 +0300

    weekly tasks

---
+++ src/app/components/weekly-tasks/weekly-tasks.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weekly-tasks',
+  imports: [],
+  templateUrl: './weekly-tasks.component.html',
+  styleUrl: './weekly-tasks.component.scss'
+})
+export class WeeklyTasksComponent {
+
+}
