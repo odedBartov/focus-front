26f8b4064f668f29e78c6232d8ece91c6a0f1406
Oded Bartov <odedoded777@gmail.com>
2025-08-13 14:58:08 +0300

    speed insight

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 // Initialize Vercel Analytics
 inject();
+injectSpeedInsights();
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
