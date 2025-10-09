711d66c59e40c0e8fea6fa0d7d61319b45fc93d5
Oded Bartov <odedoded777@gmail.com>
2025-10-09 11:58:21 +0300

    margin

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     apiUrl: 'https://localhost:4300/api/',
     realApi: 'https://focus-server-51qa.onrender.com/api/',
-    subscriptionUrl: 'https://arieladler.co.il/focus/#3/',
+    subscriptionUrl: 'https://arieladler.co.il/focus/',
     defaultPicture: 'assets/icons/default_profile.svg',
     arielsNumber: "972584046213"
   };
