0d3ab1cfbe9a057bd3de7e81ed3148bead4caede
Oded Bartov <odedoded777@gmail.com>
2025-10-24 00:25:40 +0300

    drag today tasks

--- src/app/components/updates/updates.component.ts
+++ src/app/components/updates/updates.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, effect, EventEmitter, inject, OnInit, Output, WritableSignal } from '@angular/core';
+import { AfterViewInit, Component, effect, ElementRef, EventEmitter, inject, NgZone, OnInit, Output, ViewChild, WritableSignal } from '@angular/core';
 import { HttpService } from '../../services/http.service';
 import { Feature } from '../../models/feature';
 import { AnimationsService } from '../../services/animations.service';
@@ -14,19 +14,21 @@ import { areDatesEqual } from '../../helpers/functions';
 import { WeeklyDayTaskComponent } from '../weekly-day-task/weekly-day-task.component';
 import { StepTask } from '../../models/stepTask';
 import { initRetainerSteps } from '../../helpers/retainerFunctions';
+import { CdkDragDrop, DragDropModule, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-updates',
-  imports: [CommonModule, WeeklyDayTaskComponent],
+  imports: [CommonModule, WeeklyDayTaskComponent, DragDropModule],
   templateUrl: './updates.component.html',
   styleUrl: './updates.component.scss'
 })
-export class UpdatesComponent implements OnInit {
+export class UpdatesComponent implements OnInit, AfterViewInit {
   httpService = inject(HttpService);
   animationsService = inject(AnimationsService);
   authenticationService = inject(AuthenticationService);
   updatesService = inject(UpdatesService);
   projectsService = inject(ProjectsService);
+  @ViewChild('todayTasks') todayTasksElement!: ElementRef<HTMLDivElement>;
   @Output() navigateToCalendarEmitter = new EventEmitter<void>();
   @Output() selectProject = new EventEmitter<Project>();
   projects: WritableSignal<Project[]>;
@@ -37,7 +39,7 @@ export class UpdatesComponent implements OnInit {
   fullName = "משתמש ללא שם";
   arielsNumber = environment.arielsNumber;
 
-  constructor() {
+  constructor(private ngZone: NgZone) {
     this.projects = this.projectsService.getActiveProjects();
     this.noProject = this.projectsService.getNoProject();
     effect(() => {
@@ -71,13 +73,53 @@ export class UpdatesComponent implements OnInit {
 
     const activeSteps = this.projects().flatMap(p => p.steps.filter(s => s.isRecurring));
     const retainerSteps: Step[] = [];
-    initRetainerSteps(activeSteps, retainerSteps, [], []);    
+    initRetainerSteps(activeSteps, retainerSteps, [], []);
     const retainerStepsOrTasks = retainerSteps.map(step => {
       const stepOrTask = new StepOrTask();
       stepOrTask.step = step;
       return stepOrTask;
     });
     this.stepsAndTasks = this.stepsAndTasks.concat(retainerStepsOrTasks);
+  }
+
+  ngAfterViewInit(): void {
+    this.ngZone.onStable.asObservable().pipe().subscribe(() => {
+      setTimeout(() => {
+        const width = this.todayTasksElement.nativeElement.offsetWidth;
+        document.documentElement.style.setProperty('--task-width', `${width - 50}px`); // 50 is the margin
+      }, 1);
+    });
+  }
+
+  dropStep(event: CdkDragDrop<StepOrTask[]>) {
+    moveItemInArray(this.stepsAndTasks, event.previousIndex, event.currentIndex);
+    this.updateStepsPosition();
+    const stepsToUpdate: Step[] = [];
+    this.stepsAndTasks.forEach(taskOrStep => {
+      if (!stepsToUpdate.find(s => s.id === taskOrStep.parentStep?.id)) {
+        if (taskOrStep.step) {
+          taskOrStep.parentStep = taskOrStep.step;
+        }
+        stepsToUpdate.push(taskOrStep.parentStep);
+      }
+    });
+
+    this.animationsService.changeIsLoadingWithDelay();
+    this.httpService.updateSteps(stepsToUpdate).subscribe(res => {
+      this.animationsService.changeIsloading(false);
+    })
+  }
+
+
+  updateStepsPosition() {
+    for (let index = 0; index < this.stepsAndTasks.length; index++) {
+      const task = this.stepsAndTasks[index];
+      if (task.task) {
+        task.task.positionInWeeklyList = index;
+      } else {
+        task.step!.positionInWeeklyList = index;
+      }
+    }
   }
 
   navigateToCalendar() {
