e62b8f133c5ee0edd930e3c19509d77c6a81bbf5
Oded Bartov <odedoded777@gmail.com>
2025-12-09 18:43:06 +0200

    20 seconds

--- src/app/services/version-updates.service.ts
+++ src/app/services/version-updates.service.ts
@@ -21,7 +21,7 @@ export class VersionUpdatesService {
       const appIsStable$ = this.appRef.isStable.pipe(first(isStable => isStable === true));
       // Set the interval for checking (e.g., 6 hours = 6 * 60 * 60 * 1000)
       const everySixHours$ = interval(6 * 60 * 60 * 1000);
-      const twoMinutes$ = interval(30 * 1000);
+      const twoMinutes$ = interval(20 * 1000);
 
       // Concatenate: Check immediately after stable, then check every 6 hours
       const everySixHoursOnceAppIsStable$ = concat(appIsStable$, twoMinutes$);
